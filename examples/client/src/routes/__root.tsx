76d4f1b7aa0d16eebb0b2cfb8bec39d564618b23
Matheus Patrick <mpatrickaires@gmail.com>
2025-09-15 19:34:53 -0300

    build menu items dynamically

--- examples/client/src/routes/__root.tsx
+++ examples/client/src/routes/__root.tsx
@@ -1,4 +1,9 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import {
+  createRootRoute,
+  Link,
+  Outlet,
+  useRouter,
+} from '@tanstack/react-router';
 import tanStackQueryLogo from '/tanstack-query-logo.png';
 import {
   AppBar,
@@ -41,71 +46,71 @@ const queryClient = new QueryClient({
 });
 
 export const Route = createRootRoute({
-  component: () => (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Box display="flex">
-          <AppBar
-            position="fixed"
-            sx={{ zIndex: theme => theme.zIndex.drawer + 1 }}
-          >
-            <Toolbar>
-              <Link to="/">
-                <Box display="flex" gap={2}>
-                  <img src={tanStackQueryLogo} width={32} />
-                  <Typography variant="h6" noWrap component="div">
-                    TanStack Query Examples
-                  </Typography>
-                </Box>
-              </Link>
-            </Toolbar>
-          </AppBar>
-          <Drawer
-            variant="permanent"
-            sx={{
-              width: drawerWidth,
-              flexShrink: 0,
-              [`& .MuiDrawer-paper`]: {
+  component: () => {
+    const { flatRoutes } = useRouter();
+    const allPaths: string[] = (flatRoutes.map(({ path }) => path) as string[])
+      .filter(path => path !== '/')
+      .sort();
+    console.log(allPaths);
+
+    return (
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box display="flex">
+            <AppBar
+              position="fixed"
+              sx={{ zIndex: theme => theme.zIndex.drawer + 1 }}
+            >
+              <Toolbar>
+                <Link to="/">
+                  <Box display="flex" gap={2}>
+                    <img src={tanStackQueryLogo} width={32} />
+                    <Typography variant="h6" noWrap component="div">
+                      TanStack Query Examples
+                    </Typography>
+                  </Box>
+                </Link>
+              </Toolbar>
+            </AppBar>
+            <Drawer
+              variant="permanent"
+              sx={{
                 width: drawerWidth,
-                boxSizing: 'border-box',
-              },
-            }}
-          >
-            <Toolbar />
-            <Box overflow="auto">
-              <List>
-                <ListItem>
-                  <Link to="/manual-parallel-queries">
-                    <ListItemButton>
-                      <ListItemText>Manual Parallel Queries</ListItemText>
-                    </ListItemButton>
-                  </Link>
-                </ListItem>
-                <ListItem>
-                  <Link to="/dynamic-parallel-queries">
-                    <ListItemButton>
-                      <ListItemText>Dynamic Parallel Queries</ListItemText>
-                    </ListItemButton>
-                  </Link>
-                </ListItem>
-                <ListItem>
-                  <Link to="/deduping">
-                    <ListItemButton>
-                      <ListItemText>Deduping</ListItemText>
-                    </ListItemButton>
-                  </Link>
-                </ListItem>
-              </List>
+                flexShrink: 0,
+                [`& .MuiDrawer-paper`]: {
+                  width: drawerWidth,
+                  boxSizing: 'border-box',
+                },
+              }}
+            >
+              <Toolbar />
+              <Box overflow="auto">
+                <List>
+                  {allPaths.map(path => (
+                    <ListItem>
+                      <Link to={path}>
+                        <ListItemButton>
+                          <ListItemText>
+                            <Typography textTransform="capitalize">
+                              {path.replaceAll('-', ' ')}
+                            </Typography>
+                          </ListItemText>
+                        </ListItemButton>
+                      </Link>
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+            </Drawer>
+            <Box component="main" flexGrow={1} p={3}>
+              <Toolbar />
+              <Outlet />
             </Box>
-          </Drawer>
-          <Box component="main" flexGrow={1} p={3}>
-            <Toolbar />
-            <Outlet />
           </Box>
-        </Box>
-        <ReactQueryDevtools />
-      </ThemeProvider>
-    </QueryClientProvider>
-  ),
+          <ReactQueryDevtools />
+        </ThemeProvider>
+      </QueryClientProvider>
+    );
+  },
 });
