af61f51b26bf22395f33932db42a8f5ff3229c05
Matheus Patrick <mpatrickaires@gmail.com>
2025-09-30 21:34:06 -0300

    disable cache on server

--- examples/server/app.js
+++ examples/server/app.js
@@ -5,8 +5,21 @@ import { router as parallelQueriesDynamicRouter } from './routes/parallel-querie
 import { router as dedupingRouter } from './routes/deduping.js';
 import { router as backgroundFetchingIndicator } from './routes/background-fetching-indicators.js';
 import { router as queryRetries } from './routes/query-retries.js';
+import { router as paginatedQueries } from './routes/paginated-queries.js';
 
 const app = express();
+
+// Disable cache to prevent unexpected behavior on examples
+app.use((req, res, next) => {
+  res.setHeader(
+    'Cache-Control',
+    'no-store, no-cache, must-revalidate, proxy-revalidate'
+  );
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Surrogate-Control', 'no-store');
+  next();
+});
 
 app.use(cors());
 
@@ -15,5 +28,6 @@ app.use('/parallel-queries-dynamic', parallelQueriesDynamicRouter);
 app.use('/deduping', dedupingRouter);
 app.use('/background-fetching-indicators', backgroundFetchingIndicator);
 app.use('/query-retries', queryRetries);
+app.use('/paginated-queries', paginatedQueries);
 
 app.listen(5000, () => console.log('Server running'));
