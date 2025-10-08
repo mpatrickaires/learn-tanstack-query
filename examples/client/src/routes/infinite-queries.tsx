2cc10fc93dc28a9ac968926a275e55ea3a1eff52
Matheus Patrick <mpatrickaires@gmail.com>
2025-10-08 19:19:08 -0300

    infinite queries - max pages example

--- examples/client/src/routes/infinite-queries.tsx
+++ examples/client/src/routes/infinite-queries.tsx
@@ -1,12 +1,16 @@
 import { Box, Button, Typography } from '@mui/material';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import {
+  useInfiniteQuery,
+  type InfiniteData,
+  type UseInfiniteQueryResult,
+} from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 import { useEffect, useRef, useState } from 'react';
 import { buildApi } from '../api';
 import { TabExampleContainer } from '../components/example/TabExampleContainer';
+import { VerticalSeparator } from '../components/VerticalSeparator';
 import { useExampleKey } from '../contexts/exampleKeyContext';
 import { useOnScroll } from '../hooks/useOnScroll';
-import { VerticalSeparator } from '../components/VerticalSeparator';
 
 const api = buildApi('/infinite-queries');
 
@@ -30,6 +34,12 @@ function RouteComponent() {
             'A stale infinite query will refetch sequentially, starting from the first page. In the example below, only 3 pages are fetched, and once the query becomes stale (immediately by default), when a refetch is triggered the pages will be refetched sequentially, as can be seen by the indicator and in the Network tab of DevTools.',
           component: <SequentialRefetchExample />,
         },
+        {
+          label: 'Max Pages',
+          description: `By passing \`maxPages\`, the query data will be limited to the given value, removing the smallest page number. 
+          For the following example, the \`maxPages\` is 3, which means that when reaching page 4, the page 1 will be removed, and so go on. This can be checked by scrolling up.`,
+          component: <MaxPagesExample />,
+        },
       ]}
       docsUrl="https://tanstack.com/query/latest/docs/framework/react/guides/infinite-queries"
     />
@@ -41,13 +51,45 @@ function CancelRefetchExample() {
     <Box display="flex" gap={2}>
       <Box>
         <Typography variant="h6">cancelRefetch = true</Typography>
-        <InfiniteScroll cancelRefetch={true} />
+        <CancelRefetchExampleItem cancelRefetch={true} />
       </Box>
       <VerticalSeparator />
       <Box>
         <Typography variant="h6">cancelRefetch = false</Typography>
-        <InfiniteScroll cancelRefetch={false} />
+        <CancelRefetchExampleItem cancelRefetch={false} />
       </Box>
+    </Box>
+  );
+}
+
+function CancelRefetchExampleItem({
+  cancelRefetch,
+}: {
+  cancelRefetch: boolean;
+}) {
+  const exampleKey = useExampleKey();
+  const [ongoingRefetchCount, setOngoingRefetchCount] = useState(0);
+
+  const query = useInfiniteQuery({
+    queryKey: ['infiniteQuery', 'cancelRefetch', cancelRefetch, exampleKey],
+    queryFn: async ({ pageParam }) => {
+      setOngoingRefetchCount(v => v + 1);
+      const result = await api.get<ApiData>(`/cancel-refetch/${pageParam}`);
+      setOngoingRefetchCount(v => v - 1);
+      return result.data;
+    },
+    initialPageParam: 1,
+    getNextPageParam: (_lastPage, _allPages, lastPageParam) => {
+      return lastPageParam + 1;
+    },
+  });
+
+  return (
+    <Box>
+      <Box visibility={query.isFetchingNextPage ? 'visible' : 'hidden'}>
+        <Typography>Ongoing refetch: {ongoingRefetchCount}</Typography>
+      </Box>
+      <InfiniteScroll query={query} cancelRefetch={cancelRefetch} />
     </Box>
   );
 }
@@ -68,14 +110,12 @@ function SequentialRefetchExample() {
     queryFn: async ({ pageParam }) => {
       setFetchingPageNumber(pageParam);
 
-      const result = await api.get<string[]>(
-        `/sequential-refetch/${pageParam}`
-      );
+      const result = await api.get<ApiData>(`/sequential-refetch/${pageParam}`);
       return result.data;
     },
     initialPageParam: 1,
-    getNextPageParam: (_lastPage, _allPages, lastPageParam) => {
-      return lastPageParam + 1;
+    getNextPageParam: lastPage => {
+      return lastPage.page + 1;
     },
   });
 
@@ -104,15 +144,15 @@ function SequentialRefetchExample() {
         Fetching Page {fetchingPageNumber}
       </Typography>
       <Box display="flex" gap={2}>
-        {data.pages.map((page, i) => (
-          <Box display="flex" gap={2} key={`page-${i}`}>
+        {data.pages.map(({ page, result }) => (
+          <Box display="flex" gap={2} key={`page-${page}`}>
             <Box>
-              <Typography>Page {i + 1}</Typography>
-              {page.map(item => (
+              <Typography>Page {page}</Typography>
+              {result.map(item => (
                 <Typography key={item}>- {item}</Typography>
               ))}
             </Box>
-            {i + 1 < data.pages.length && <VerticalSeparator />}
+            {page < data.pages.length && <VerticalSeparator />}
           </Box>
         ))}
       </Box>
@@ -120,37 +160,41 @@ function SequentialRefetchExample() {
   );
 }
 
-function InfiniteScroll({ cancelRefetch }: { cancelRefetch: boolean }) {
+function MaxPagesExample() {
   const exampleKey = useExampleKey();
-  const [ongoingRefetchCount, setOngoingRefetchCount] = useState(0);
 
-  const { data, isLoading, isSuccess, fetchNextPage, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryKey: ['infiniteQuery', 'cancelRefetch', cancelRefetch, exampleKey],
-      queryFn: async ({ pageParam }) => {
-        setOngoingRefetchCount(v => v + 1);
-        const result = await api.get<string[]>(`/cancel-refetch/${pageParam}`);
-        setOngoingRefetchCount(v => v - 1);
-        return result.data;
-      },
-      initialPageParam: 1,
-      getNextPageParam: (_lastPage, _allPages, lastPageParam) => {
-        return lastPageParam + 1;
-      },
-    });
+  const query = useInfiniteQuery({
+    queryKey: ['infiniteQueries', 'maxPages', exampleKey],
+    queryFn: ({ pageParam }) =>
+      api.get(`/max-pages/${pageParam}`).then(r => r.data),
+    maxPages: 3,
+    initialPageParam: 1,
+    getNextPageParam: (_lastPage, _allPages, lastPageParam) => {
+      return lastPageParam + 1;
+    },
+  });
 
+  return <InfiniteScroll query={query} cancelRefetch={false} />;
+}
+
+function InfiniteScroll({
+  query: { fetchNextPage, data, isFetchingNextPage, isLoading },
+  cancelRefetch,
+}: {
+  query: UseInfiniteQueryResult<InfiniteData<ApiData, unknown>>;
+  cancelRefetch: boolean;
+}) {
   const ref = useRef<HTMLElement | null>(null);
   useOnScroll({
+    offsetBottom: 10,
     ref,
     onReachBottom: () => fetchNextPage({ cancelRefetch }),
-    offsetBottom: 20,
   });
 
   return (
     <Box>
       <Box visibility={isFetchingNextPage ? 'visible' : 'hidden'}>
         <Typography>Fetching next page...</Typography>
-        <Typography>Ongoing refetch: {ongoingRefetchCount}</Typography>
       </Box>
       <Box
         maxHeight={120}
@@ -160,15 +204,14 @@ function InfiniteScroll({ cancelRefetch }: { cancelRefetch: boolean }) {
         border="1px dashed darkseagreen"
         ref={ref}
       >
-        {isSuccess &&
-          data.pages.map((page, i) => (
-            <>
-              <Typography>Page {i + 1}</Typography>
-              {page.map(item => (
-                <Typography>- {item}</Typography>
-              ))}
-            </>
-          ))}
+        {data?.pages.map(({ page, result }) => (
+          <Box key={`page-${page}`}>
+            <Typography fontWeight={500}>Page {page}</Typography>
+            {result.map(item => (
+              <Typography>- {item}</Typography>
+            ))}
+          </Box>
+        ))}
         <Typography textAlign="center">
           {isLoading ? 'Initial loading...' : '. . .'}
         </Typography>
@@ -176,3 +219,8 @@ function InfiniteScroll({ cancelRefetch }: { cancelRefetch: boolean }) {
     </Box>
   );
 }
+
+type ApiData = {
+  page: number;
+  result: string[];
+};
--- examples/server/routes/infinite-queries.js
+++ examples/server/routes/infinite-queries.js
@@ -4,17 +4,29 @@ import { buildPaginatedResult } from '../utils.js';
 const router = Router();
 
 router.get('/cancel-refetch/:page', async (req, res) => {
+  const page = Number(req.params.page);
+
   const result = await buildPaginatedResult(req.params.page, 15);
 
-  return res.send(result);
+  return res.send({ page, result });
 });
 
 router.get('/sequential-refetch/:page', async (req, res) => {
+  const page = Number(req.params.page);
+
   const result = (await buildPaginatedResult(req.params.page, 5)).map(
     value => `${value} (${new Date().toLocaleTimeString('pt-BR')})`
   );
 
-  return res.send(result);
+  return res.send({ page, result });
+});
+
+router.get('/max-pages/:page', async (req, res) => {
+  const page = Number(req.params.page);
+
+  const result = await buildPaginatedResult(page, 5);
+
+  return res.send({ page, result });
 });
 
 export { router };
